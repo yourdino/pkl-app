c48f57f29ab0410a85d123008e4ae24bd1d505db
yourdino <mayaayun167@gmail.com>
2025-05-31 10:24:39 +0700

    model pkl

--- app/Models/pkl.php
+++ app/Models/pkl.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Pkl extends Model
+class pkl extends Model
 {
     protected $fillable = ['siswa_id', 'guru_id', 'industri_id', 'mulai', 'selesai'];
 
